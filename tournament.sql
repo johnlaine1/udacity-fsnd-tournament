a84f2feec7eb16ea3a9ad117a1c16e0dd0410496
John Laine <john.laine@centurylink.net>
2016-09-20 12:06:22 -0700

    docs: Add code comments

--- tournament.sql
+++ tournament.sql
@@ -1,18 +1,11 @@
--- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
+-- Table and View definitions for the tournament project.
 
 \c tournament
 -- Order is very important here, because of dependencies.
--- Get the order wrong and you will get an error.
+-- If the order is wrong and you will get an error.
 DROP VIEW IF EXISTS player_standings;
 DROP VIEW IF EXISTS matches_by_player;
 DROP VIEW IF EXISTS wins_by_player;
-
 DROP TABLE IF EXISTS matches;
 DROP TABLE IF EXISTS players;
 
@@ -22,27 +15,28 @@ CREATE TABLE players (
   name TEXT
 );
 
-
 CREATE TABLE matches (
   id SERIAL,
   winner INTEGER REFERENCES players(id),
   loser INTEGER REFERENCES players(id)
 );
 
-
+-- Returns a list of players and the number of matches played.
 CREATE VIEW matches_by_player AS
   SELECT p.id, p.name, count(m.id) as matches
   FROM players as p
   LEFT JOIN matches as m ON p.id = m.winner or p.id = m.loser
   GROUP BY p.id;
 
-
+-- Returns a list of players and the number of matches won.
 CREATE VIEW wins_by_player AS
   SELECT p.id, p.name, count(m.winner) as wins
   FROM players as p
   LEFT JOIN matches as m ON m.winner = p.id 
   GROUP BY p.id;
  
+-- Returns a list of players with the number of matches played and the
+-- number of matches won.
 CREATE VIEW player_standings AS
   SELECT w.id, w.name, w.wins, m.matches 
   FROM wins_by_player as w
